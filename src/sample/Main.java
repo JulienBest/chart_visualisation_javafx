09b1e1f22d3971d28a2e0f87e22d49831a4b1ef8
Julien <julien.best@live.de>
2020-03-05 20:09:12 +0100

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private final int WIDTH = 1400;
+    private final int HEIGHT = 800;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Pane root = new Pane();
+        primaryStage.setTitle("Chart Generator");
+        Scene scene = new Scene(root, WIDTH, HEIGHT, Color.grayRgb(30));
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
